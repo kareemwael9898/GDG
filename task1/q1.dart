9125588b6d21ea10406432daa308b29ec9b0fec2
kareemwael9898 <kareemwael9898@gmail.com>
2025-02-13 15:07:56 +0200

    finished q1

--- task1/q1.dart
+++ task1/q1.dart
@@ -1,3 +1,13 @@
-void main(){
-    
+import 'dart:io';
+
+void main() {
+  print("Enter a number: ");
+  int number = int.parse(stdin.readLineSync() ?? '');
+  bool isOdd = (number % 2 == 1) ? true : false;
+
+  if (isOdd) {
+    print("The number is odd");
+  } else {
+    print("The number is even");
+  }
 }
