d8cd6ec29674c4a7a014c720946f14b1049f5918
kareemwael9898 <kareemwael9898@gmail.com>
2025-02-13 15:38:11 +0200

    finished q4

--- task1/q4.dart
+++ task1/q4.dart
@@ -1,3 +1,21 @@
-void main(){
-    
+/*Create a Dart class Car with the following properties: brand, model, and year.
+Add a method displayInfo() that prints out the car details. Then, create an
+object of this class and call the method to display the information.*/
+void main() {
+  Car car = Car(brand: "BMW", model: "XM", year: "2025");
+
+  car.displayInfo();
+}
+
+class Car {
+  String brand;
+  String model;
+  String year;
+
+  Car({required this.brand, required this.model, required this.year});
+
+  void displayInfo(){
+    // print("Car Details: Brand - $brand, Model - $model, Year - $year");
+    print("Car Details: Brand: $brand, Model: $model, Year: $year");
+  }
 }
